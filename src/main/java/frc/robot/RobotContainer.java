43c84943ef2aceffe6ca762e5453b9942588224a
Jadyn-Mithani <109890418+Jadyn-Mithani@users.noreply.github.com>
2025-01-15 12:03:58 -0800

    Revert "Got PhotonVision Handler done"
    This reverts commit b87ab4f1a207f9eec0326ea71bd663cf61fc47d5.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,61 +6,24 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.photonvision.EstimatedRobotPose;
-import org.photonvision.targeting.PhotonTrackedTarget;
-
-import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.PowerDistribution;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-import org.photonvision.EstimatedRobotPose;
-import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import org.photonvision.PhotonUtils;
-import org.photonvision.targeting.PhotonTrackedTarget;
-
 
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
-import frc.robot.util.PhotonVisionHandler;
-// import frc.robot.Vision.MeasurementInfo;
-
 
 public class RobotContainer {
 
         // 6 meters per second desired top speed.
     private static final double kMaxSpeed = 0.2;
-    PowerDistribution m_powerdistro = new PowerDistribution();
-
-    private Optional<EstimatedRobotPose> prevVisionOut = Optional.empty();
-    private Optional<EstimatedRobotPose> Visionout;
-    private final Field2d m_Visionpose = new Field2d();
-
-    public final PhotonVisionHandler visionHandler = new PhotonVisionHandler();
-    AprilTagFieldLayout aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
-    // Vision visionInstance;
-
-
-
 
     // Half a rotation per second max angular velocity.
     private static final double kMaxAngularRate = 0.2 * Math.PI;
@@ -104,7 +67,6 @@ public class RobotContainer {
 
     public RobotContainer() {
         configureBindings();
-        m_powerdistro.setSwitchableChannel(true);
     }
     
 
@@ -129,75 +91,6 @@ public class RobotContainer {
 
         m_drivetrain.registerTelemetry(logger::telemeterize);
     }
-
-
-    public void updatePoseEstimator() {
-
-    if (aprilTagFieldLayout == null) {
-      System.err.println("AprilTagFieldLayout is null. Skipping pose estimation update.");
-      return;
-    }
-
-    double lateralDeviation; // standard deviation of the x and y measurements
-    double angularDeviation; // standard deviation of the angle measurement
-    // final MeasurementInfo internalTag =
-    // visionInstance.new MeasurementInfo(visionHandler.getAprilTagID(),
-    // visionHandler.getNumberofTags(), visionHandler.areaOfAprilTag());
-
-    if (prevVisionOut.isPresent()) {
-      Visionout =
-          visionHandler.getEstimatedGlobalPose(prevVisionOut.get().estimatedPose.toPose2d());
-    } else {
-      Visionout = visionHandler
-          .getEstimatedGlobalPose(new Pose2d(new Translation2d(0, 0), new Rotation2d(0)));
-    }
-    prevVisionOut = Visionout;
-
-    if (Visionout.isPresent()) {
-
-      final Pose2d visPose = Visionout.get().estimatedPose.toPose2d();
-      final double posDiff = m_drivetrain.getPoseDifference(visPose);
-
-      final List<PhotonTrackedTarget> tags = Visionout.get().targetsUsed;
-
-
-      // return if no tag detected
-      if (tags.size() < 1) {
-        return;
-      }
-      // more than 1 tag in view
-      if (tags.size() > 1 && visionHandler.avgTagArea(tags) > 80) {
-        lateralDeviation = 0.5;
-        angularDeviation = 6;
-      }
-      // 1 target with large area and close to estimated pose
-      else if (tags.get(0).getArea() > 80 && posDiff < 0.5) {
-        lateralDeviation = 1.0;
-        angularDeviation = 12;
-      }
-      // 1 target farther away and estimated pose is close
-      else if (tags.get(0).getArea() > 10 && posDiff < 0.3) {
-        lateralDeviation = 2.0;
-        angularDeviation = 30;
-      }
-      // conditions don't match to add a vision measurement
-      else
-        return;
-
-      m_Visionpose.setRobotPose(Visionout.get().estimatedPose.toPose2d());
-
-      if (Utils.isSimulation() == true)
-
-      {
-
-        Pose2d visPose2d = Visionout.get().estimatedPose.toPose2d();
-        double visionstamp = Visionout.get().timestampSeconds;
-        m_drivetrain.addVisionMeasurement(visPose2d, visionstamp, VecBuilder.fill(lateralDeviation,
-            lateralDeviation, Units.degreesToRadians(angularDeviation)));
-      }
-    }
-  }
-
 
     public Command getAutonomousCommand() {
         return Commands.print("No autonomous command configured");
