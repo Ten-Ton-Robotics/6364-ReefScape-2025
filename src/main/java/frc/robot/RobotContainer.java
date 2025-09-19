45184d0e75a5a104f3d3e503dd25c4e4ac3d30cb
TenTonFRC <jadyn.mithani@gmail.com>
2025-09-19 16:03:15 -0700

    Redoing Testing
    - Alot of stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -35,6 +35,61 @@ public class RobotContainer {
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
+<<<<<<< Updated upstream
+=======
+    public void init(){
+      objectDetected.onTrue(m_Intake.coralControlCommand(0.055)); //-0.38
+      objectDetected.onFalse(m_Intake.forwards(true));
+
+      m_Arm.goToAngle(0.26).schedule();
+
+      if (!objectDetected.getAsBoolean()) {
+        m_Intake.forwards(true).alongWith(m_Arm.goToAngle(0.26)).schedule();
+      }
+    }
+ 
+    private double expoCurve(final double input, final double a, final double deadband) {
+        final double absinput = Math.abs(input);
+        final double inverseA = (1.0 / a);
+        final double s_deadband = (deadband * Math.signum(input));
+        final double inversemax = 1.0 / (1.0 + deadband);
+
+        if (absinput < deadband) {
+            return 0;
+        }
+
+        return (((Math.pow(a, absinput) * input * inverseA) + s_deadband) * inversemax);
+    }
+
+    private double getFieldCentricAngleFromJoystick(final double x, final double y, final double deadzone){
+      final double magnitude = Math.hypot(x, y);
+      boolean firstrun = true;
+      double defaultangle = 0;
+
+      if(firstrun){
+        firstrun = false;
+        defaultangle = m_drivetrain.getPose2d().getRotation().getRadians();
+      }
+
+      if(magnitude > deadzone){
+
+        final double idealy = Math.sqrt((1-Math.pow(x, 2)));
+        double rawangle = Math.atan2(idealy, x);
+
+        if(y < 0){
+            rawangle = Math.PI + rawangle;
+        }
+
+        defaultangle = rawangle;
+
+
+      }
+
+      return defaultangle;
+    }
+
+    
+>>>>>>> Stashed changes
     public RobotContainer() {
         configureBindings();
     }
@@ -66,7 +121,55 @@ public class RobotContainer {
         // reset the field-centric heading on left bumper press
         joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
+<<<<<<< Updated upstream
         drivetrain.registerTelemetry(logger::telemeterize);
+=======
+        m_controller.x().onTrue(l4Command());
+
+        //m_controller.leftStick().onTrue(m_drivetrain.findAndFollowPath(new Pose2d(5.2619, 4.99953, Rotation2d.fromDegrees(240)))); // 20 Left
+
+        //m_controller.rightStick().onTrue(m_drivetrain.findAndFollowPath(new Pose2d(5.2619, 3.05047, Rotation2d.fromDegrees(120)))); // 20 Right
+
+        m_controller.rightBumper().onTrue(algaeclearTop());
+        // new Pose2d(4.05, 2.95, Rotation2d.fromDegrees(60)), // 17 Right
+
+        m_controller.leftBumper().onTrue(algaeclearBottom());
+        
+        m_controller.povRight().onTrue(algueScoreCmd()); 
+        
+        m_controller.povLeft().onTrue(algaeOutCmd());  
+        // m_controller.povLeft().onTrue(m_Intake.reversesame());
+
+        m_controller.povDown().onTrue(m_Intake.stop());
+
+        m_controller.povUp().onTrue(rampRelease(0.5).andThen(climberGoUp()));
+
+        m_controller.rightTrigger()
+        .whileTrue(climberControlLogic())
+        .onFalse(m_climber.stop());
+
+
+        m_controller.leftTrigger()
+        .onTrue(m_Arm.goToAngle(loadangle).andThen(m_Intake.forwards(false).withTimeout(1)))
+        .onFalse(resetElevatorCmd());
+
+        // CTRE Logger
+        m_drivetrain.registerTelemetry(logger::telemeterize);
+
+   }
+
+
+    // Pose estimator update logic (meant to increase accuracy by filtering out bad or unusable output from the Cameras)
+    public void updatePoseEstimator() {
+      rightPoseEstimator.updatePose("Right Side Pose");
+      leftPoseEstimator.updatePose("Left Side Pose");
+      SmartDashboard.putBoolean("Coral Trigger", objectDetected.getAsBoolean());
+
+      // Display Robot Pose on shuffleboard
+      m_FieldPose.setRobotPose(m_drivetrain.getPose2d());
+      SmartDashboard.putData("RobotPose Field2D", m_FieldPose);
+    
+>>>>>>> Stashed changes
     }
 
     public Command getAutonomousCommand() {
